bbcfd0a2c8ccc2840dd00ccfb98775f9d5db67bd
Oleksandr Kamniev <oleksandr.kamniev@kopentech.com>
2022-01-31 12:39:46 +0300

    update namespaces

--- src/Module.php
+++ src/Module.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\src\demomodule;
+namespace akam\demomodule;
 
 class Module extends \yii\base\Module
 {
--- src/controllers/DemoController.php
+++ src/controllers/DemoController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\src\demomodule\controllers;
+namespace akam\demomodule\controllers;
 
 
 use Yii;
